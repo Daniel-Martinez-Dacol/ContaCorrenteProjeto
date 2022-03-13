b1f9b8092976ae42f76a14138a49d15ab2db0187
Daniel <danieldaol99@gmail.com>
2022-03-13 14:38:39 -0300

    Add project files.

---
+++ TesteContaCorrente.ConsoleApp/ContaCorrente.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoContaCorrente.ConsoleApp
+{
+    internal class ContaCorrente
+    {
+        public int numeroDaContaCorrente;
+        public decimal saldo;
+        public bool ehEspecial;
+        public decimal limite;
+        public Movimentacoes[] movimentacoes;
+
+
+        public void Saque(decimal valor)
+        {
+
+            if ((saldo + limite) > valor)
+            {
+                saldo -= valor;
+                int posicao = ContadorDeMovimentacoes();
+                Movimentacoes movSaque = new Movimentacoes();
+                movSaque.valor = valor;
+                movSaque.tipo = "Debito";
+                movimentacoes[posicao] = movSaque;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERRO: Saque indisponivel devido limite insuficiente!");
+                Console.ResetColor();
+            }
+
+
+        }
+        public void Deposito(decimal valor)
+        {
+            int posicao = ContadorDeMovimentacoes();
+            saldo += valor;
+            Movimentacoes movDeposito = new Movimentacoes();
+            movDeposito.valor = valor;
+            movDeposito.tipo = "Debito";
+            movimentacoes[posicao] = movDeposito;
+
+
+        }
+
+        public void Transferencia(ContaCorrente Conta, decimal valor)
+        {
+
+            if ((saldo + limite) > valor)
+            {
+                int posicao = ContadorDeMovimentacoes();
+                this.saldo -= valor;
+                Conta.saldo += valor;
+                Movimentacoes movTransfere = new Movimentacoes();
+                movTransfere.valor = valor;
+                movTransfere.tipo = "Debito";
+                movimentacoes[posicao] = movTransfere;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERRO: Transferencia indisponivel devido a saldo ou limite insuficiente!");
+                Console.ResetColor();
+            }
+
+
+        }
+        public void Extrato()
+        {
+
+            string tipoDeConta;
+            if (this.ehEspecial == true)
+            {
+                tipoDeConta = "Espcial";
+            }
+            else
+            {
+                tipoDeConta = "Normal";
+            }
+            Console.WriteLine("_______________________________________EXTRATO:___________________________________________\n");
+            Console.WriteLine($"-Numero da conta: {this.numeroDaContaCorrente}\n" +
+                $"-Saldo da conta: {this.saldo}\n-Tipo de conta: {tipoDeConta}\n" +
+                $"-Limite da conta: {this.limite} ");
+            Console.WriteLine("________________________________MOVIMENTAÇÕES DA CONTA:____________________________________\n");
+            for (int i = 0; i < movimentacoes.Length; i++)
+            {
+                if (movimentacoes[i] != null)
+                {
+                    Console.WriteLine($"-Movimentação:\n-Tipo da movimentção: {movimentacoes[i].tipo}\n-Valor da movimentação:{movimentacoes[i].valor}\n");
+                    Console.WriteLine("____________________________________________________________________________________\n");
+                }
+                
+            }
+
+        }
+        public int ContadorDeMovimentacoes()
+        {
+            int posicao = -1;
+            for (int i = 0; i < movimentacoes.Length; i++)
+            {
+                if (movimentacoes[i] == null)
+                {
+                    posicao = i;
+                }
+            }
+            return posicao;
+        }
+
+    }
+    public class Movimentacoes
+    {
+        public string tipo;
+        public decimal valor;
+    }
+
+}
---
+++ TesteContaCorrente.ConsoleApp/TesteContaCorrente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ProjetoContaCorrente.ConsoleApp
+{
+    internal class TesteContaCorrente
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1 = new ContaCorrente();
+            ContaCorrente conta2 = new ContaCorrente();
+
+            conta1.numeroDaContaCorrente = 1;
+            conta1.saldo = 20000;
+            conta1.limite = 10000;
+            conta1.ehEspecial = false;
+            conta1.movimentacoes = new Movimentacoes[10];
+            
+
+            conta2.numeroDaContaCorrente = 2;
+            conta2.saldo = 15000;
+            conta2.limite = 8000;
+            conta2.ehEspecial = false;
+            conta2.movimentacoes = new Movimentacoes[10];
+
+            conta1.Saque(1000);
+            conta2.Saque(5000);
+
+            conta1.Deposito(3000);
+            conta2.Deposito(2000);
+
+            conta1.Transferencia(conta2, 1000);
+
+            conta1.Extrato();
+            conta2.Extrato();
+
+            Console.ReadLine();
+
+            Console.Clear();
+
+        }
+    }
+}
